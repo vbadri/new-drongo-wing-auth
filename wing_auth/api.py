7ce3aeea2745121bac601011fbd9ebcc7ee7e072
Badri Varadarajan <badri@dekk.io>
2019-02-14 21:01:34 -0800

    Checking in debug code

--- wing_auth/api.py
+++ wing_auth/api.py
@@ -6,7 +6,8 @@ from drongo.utils import dict2
 from drongo_utils.endpoint import APIEndpoint
 from drongo_utils.helpers import URLHelper
 
-
+import logging
+logger = logging.getLogger("auth.api")
 url = URLHelper.url
 
 
@@ -229,6 +230,7 @@ class UserLogin(APIEndpoint):
         self.query = dict2.from_dict(self.ctx.request.json)
         self.auth = self.ctx.modules.auth
 
+        logger.info("Got here with query = {}".format(self.query))
         self.login_svc = self.auth.services.UserLoginService(
             username=self.query.username,
             password=self.query.password
@@ -252,6 +254,8 @@ class UserLogin(APIEndpoint):
             self.valid = False
             self.error(message='Invalid username or password.')
             self.auth.services.UserLogoutService().call(self.ctx)
+        
+        logger.info("Validation done")
 
     def call(self):
         token = self.login_svc.create_token()
@@ -259,6 +263,7 @@ class UserLogin(APIEndpoint):
         if self.auth.config.token_in_session:
             self.login_svc.authenticate_session(self.ctx, token)
 
+        logger.info("All done, got user = {}".format(self.ctx.auth.user))
         self.status(HttpStatusCodes.HTTP_202)
 
         return {
